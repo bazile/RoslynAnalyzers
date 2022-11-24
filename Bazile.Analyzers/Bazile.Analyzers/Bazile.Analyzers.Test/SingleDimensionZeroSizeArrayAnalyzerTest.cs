3b1a715acb33d4ff8219c5f14a3b2d9f30e2f3f0
bazile <bazile@users.noreply.github.com>
2022-11-25 00:30:25 +0300

    Use file scoped namespace

--- Bazile.Analyzers/Bazile.Analyzers/Bazile.Analyzers.Test/SingleDimensionZeroSizeArrayAnalyzerTest.cs
+++ Bazile.Analyzers/Bazile.Analyzers/Bazile.Analyzers.Test/SingleDimensionZeroSizeArrayAnalyzerTest.cs
@@ -4,108 +4,107 @@ using VerifyCS = Bazile.Analyzers.Test.CSharpCodeFixVerifier<
     Bazile.Analyzers.SingleDimensionZeroSizeArrayAnalyzer,
     Bazile.Analyzers.BazileAnalyzersCodeFixProvider>;
 
-namespace Bazile.Analyzers.Test
+namespace Bazile.Analyzers.Test;
+
+[TestClass]
+public class SingleDimensionZeroSizeArrayAnalyzerTest
 {
-    [TestClass]
-    public class SingleDimensionZeroSizeArrayAnalyzerTest
+    [TestMethod]
+    public async Task SingleDimensionalArray01_VerifyCodeFixAsync()
     {
-        [TestMethod]
-        public async Task SingleDimensionalArray01_VerifyCodeFixAsync()
-        {
-            const string source = @"using System; class Program { void Foo() {
+        const string source = @"using System; class Program { void Foo() {
     int[] array = new int[0];
 }}";
 
-            // TODO: what about usings?>
-            const string codeFix = @"using System; class Program { void Foo() {
+        // TODO: what about usings?>
+        const string codeFix = @"using System; class Program { void Foo() {
     int[] array = Array.Empty<int>();
 }}";
 
-            //var expected = VerifyCS.Diagnostic("BazileAnalyzers").WithLocation(0).WithArguments("TypeName");
-            var expected = VerifyCS.Diagnostic("SingleDimensionZeroSizeArray").WithSpan(2, 19, 2, 29);
-            await VerifyCS.VerifyCodeFixAsync(source, expected, codeFix);
-        }
+        //var expected = VerifyCS.Diagnostic("BazileAnalyzers").WithLocation(0).WithArguments("TypeName");
+        var expected = VerifyCS.Diagnostic("SingleDimensionZeroSizeArray").WithSpan(2, 19, 2, 29);
+        await VerifyCS.VerifyCodeFixAsync(source, expected, codeFix);
+    }
 
-        [TestMethod]
-        public async Task SingleDimensionalArray02_VerifyCodeFixAsync()
-        {
-            const string source = @"using System; class Program { void Foo() {
+    [TestMethod]
+    public async Task SingleDimensionalArray02_VerifyCodeFixAsync()
+    {
+        const string source = @"using System; class Program { void Foo() {
     var array = new int[0];
 }}";
 
-            const string codeFix = @"using System; class Program { void Foo() {
+        const string codeFix = @"using System; class Program { void Foo() {
     var array = Array.Empty<int>();
 }}"; // what about usings?>
 
-            //var expected = VerifyCS.Diagnostic("BazileAnalyzers").WithLocation(0).WithArguments("TypeName");
-            var expected = VerifyCS.Diagnostic("SingleDimensionZeroSizeArray").WithSpan(2, 17, 2, 27);
-            await VerifyCS.VerifyCodeFixAsync(source, expected, codeFix);
-        }
+        //var expected = VerifyCS.Diagnostic("BazileAnalyzers").WithLocation(0).WithArguments("TypeName");
+        var expected = VerifyCS.Diagnostic("SingleDimensionZeroSizeArray").WithSpan(2, 17, 2, 27);
+        await VerifyCS.VerifyCodeFixAsync(source, expected, codeFix);
+    }
 
-        [TestMethod]
-        public async Task SingleDimensionalArray03_VerifyCodeFixAsync()
-        {
-            const string source = @"using System; class Program { void Foo(int[] array) {
+    [TestMethod]
+    public async Task SingleDimensionalArray03_VerifyCodeFixAsync()
+    {
+        const string source = @"using System; class Program { void Foo(int[] array) {
     Foo(new int[0]);
 }}";
 
-            const string codeFix = @"using System; class Program { void Foo(int[] array) {
+        const string codeFix = @"using System; class Program { void Foo(int[] array) {
     Foo(Array.Empty<int>());
 }}";
 
-            //var expected = VerifyCS.Diagnostic("BazileAnalyzers").WithLocation(0).WithArguments("TypeName");
-            var expected = VerifyCS.Diagnostic("SingleDimensionZeroSizeArray").WithSpan(2, 9, 2, 19);
-            await VerifyCS.VerifyCodeFixAsync(source, expected, codeFix);
-        }
+        //var expected = VerifyCS.Diagnostic("BazileAnalyzers").WithLocation(0).WithArguments("TypeName");
+        var expected = VerifyCS.Diagnostic("SingleDimensionZeroSizeArray").WithSpan(2, 9, 2, 19);
+        await VerifyCS.VerifyCodeFixAsync(source, expected, codeFix);
+    }
 
-        [TestMethod]
-        public async Task EmptySource_Is_Ignored()
-        {
-            const string source = "";
+    [TestMethod]
+    public async Task EmptySource_Is_Ignored()
+    {
+        const string source = "";
 
-            await VerifyCS.VerifyAnalyzerAsync(source);
-        }
+        await VerifyCS.VerifyAnalyzerAsync(source);
+    }
 
-        [TestMethod]
-        public async Task SingleDimensionalArray_WithConstSize_Is_Ignored() // ???
-        {
-            const string source = @"class Program { void Foo() {
+    [TestMethod]
+    public async Task SingleDimensionalArray_WithConstSize_Is_Ignored() // ???
+    {
+        const string source = @"class Program { void Foo() {
     const int size = 0;
     int[] array = new int[size];
 }}";
 
-            await VerifyCS.VerifyAnalyzerAsync(source);
-        }
+        await VerifyCS.VerifyAnalyzerAsync(source);
+    }
 
-        [TestMethod]
-        public async Task SingleDimensionalArray_WithVariableSize_Is_Ignored()
-        {
-            const string source = @"class Program { void Foo() {
+    [TestMethod]
+    public async Task SingleDimensionalArray_WithVariableSize_Is_Ignored()
+    {
+        const string source = @"class Program { void Foo() {
     int size = int.Parse(System.Console.ReadLine());
     int[] array = new int[size];
 }}";
 
-            await VerifyCS.VerifyAnalyzerAsync(source);
-        }
+        await VerifyCS.VerifyAnalyzerAsync(source);
+    }
 
-        [TestMethod]
-        public async Task SingleDimensionalArray_WithNonZeroSize_Is_Ignored()
-        {
-            const string source = @"class Program { void Foo() {
+    [TestMethod]
+    public async Task SingleDimensionalArray_WithNonZeroSize_Is_Ignored()
+    {
+        const string source = @"class Program { void Foo() {
     int[] array = new int[5];
 }}";
 
-            await VerifyCS.VerifyAnalyzerAsync(source);
-        }
+        await VerifyCS.VerifyAnalyzerAsync(source);
+    }
 
-        [TestMethod]
-        public async Task TwoDimensionalArray_Is_Ignored()
-        {
-            const string source = @"class Program { void Foo() {
+    [TestMethod]
+    public async Task TwoDimensionalArray_Is_Ignored()
+    {
+        const string source = @"class Program { void Foo() {
     int[,] array = new int[0,0];
 }}";
 
-            await VerifyCS.VerifyAnalyzerAsync(source);
-        }
+        await VerifyCS.VerifyAnalyzerAsync(source);
     }
 }
--- Bazile.Analyzers/Bazile.Analyzers/Bazile.Analyzers/SingleDimensionZeroSizeArrayAnalyzer.cs
+++ Bazile.Analyzers/Bazile.Analyzers/Bazile.Analyzers/SingleDimensionZeroSizeArrayAnalyzer.cs
@@ -1,85 +1,83 @@
 ﻿using System.Collections.Immutable;
-using System.Data.Common;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
-namespace Bazile.Analyzers
+namespace Bazile.Analyzers;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class SingleDimensionZeroSizeArrayAnalyzer : DiagnosticAnalyzer
 {
-    [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public class SingleDimensionZeroSizeArrayAnalyzer : DiagnosticAnalyzer
+    public const string DiagnosticId = "SingleDimensionZeroSizeArray";
+    //public const string DiagnosticId = "BazileAnalyzers";
+
+    // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+    // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
+    private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+    private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+    private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+    private const string Category = "Naming"; // TODO: Change
+
+    private static readonly DiagnosticDescriptor Rule = new(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+    public override void Initialize(AnalysisContext context)
     {
-        public const string DiagnosticId = "SingleDimensionZeroSizeArray";
-        //public const string DiagnosticId = "BazileAnalyzers";
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution();
 
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
-        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
-        private const string Category = "Naming"; // TODO: Change
+        //// TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
+        //// See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
+        //context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
 
-        private static readonly DiagnosticDescriptor Rule = new(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+        context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.ArrayCreationExpression);
+    }
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
-
-        public override void Initialize(AnalysisContext context)
+    private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
+    {
+        bool shouldReport = ShouldReport(context);
+        if (shouldReport)
         {
-            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
-            context.EnableConcurrentExecution();
+            //var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
+            var diagnostic = Diagnostic.Create(Rule, context.Node.GetLocation());
+            context.ReportDiagnostic(diagnostic);
+        }
+    }
 
-            //// TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            //// See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            //context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+    //private static void AnalyzeSymbol(SymbolAnalysisContext context)
+    //{
+    //    // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
+    //    var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
 
-            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.ArrayCreationExpression);
+    //    // Find just those named type symbols with names containing lowercase letters.
+    //    if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+    //    {
+    //        // For all such symbols, produce a diagnostic.
+    //        var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
+
+    //        context.ReportDiagnostic(diagnostic);
+    //    }
+    //}
+
+    private static bool ShouldReport(SyntaxNodeAnalysisContext context)
+    {
+        bool result = false;
+
+        var arraySyntax = (ArrayCreationExpressionSyntax)context.Node;
+        var rankSpecifiers = arraySyntax.Type.RankSpecifiers;
+        var sizes = rankSpecifiers[0].Sizes;
+        if (
+            rankSpecifiers.Count == 1
+            && sizes.Count == 1
+            && sizes[0] is LiteralExpressionSyntax rankLiteralSyntax
+            && rankLiteralSyntax.Token.Kind() == SyntaxKind.NumericLiteralToken)
+        {
+            int size = (int)rankLiteralSyntax.Token.Value;
+            result = size == 0;
         }
 
-        private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
-        {
-            bool shouldReport = ShouldReport(context);
-            if (shouldReport)
-            {
-                //var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
-                var diagnostic = Diagnostic.Create(Rule, context.Node.GetLocation());
-                context.ReportDiagnostic(diagnostic);
-            }
-        }
-
-        //private static void AnalyzeSymbol(SymbolAnalysisContext context)
-        //{
-        //    // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-        //    var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
-
-        //    // Find just those named type symbols with names containing lowercase letters.
-        //    if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
-        //    {
-        //        // For all such symbols, produce a diagnostic.
-        //        var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
-
-        //        context.ReportDiagnostic(diagnostic);
-        //    }
-        //}
-
-        private static bool ShouldReport(SyntaxNodeAnalysisContext context)
-        {
-            bool result = false;
-
-            var arraySyntax = (ArrayCreationExpressionSyntax)context.Node;
-            var rankSpecifiers = arraySyntax.Type.RankSpecifiers;
-            var sizes = rankSpecifiers[0].Sizes;
-            if (
-                rankSpecifiers.Count == 1
-                && sizes.Count == 1
-                && sizes[0] is LiteralExpressionSyntax rankLiteralSyntax
-                && rankLiteralSyntax.Token.Kind() == SyntaxKind.NumericLiteralToken)
-            {
-                int size = (int)rankLiteralSyntax.Token.Value;
-                result = size == 0;
-            }
-
-            return result;
-        }
+        return result;
     }
 }
